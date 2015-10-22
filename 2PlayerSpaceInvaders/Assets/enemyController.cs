d91b329dca549e00f7d92ec28acc6dae91c12d40
unknown <SYBRANDTJG1@2CE2211R2G.GCC.edu>
2015-10-22 10:02:33 -0400

    I keep forgetting to add

---
+++ 2PlayerSpaceInvaders/Assets/enemyController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyController : MonoBehaviour {
+
+	public GameObject enemyPrefab; //better get set
+	
+	// Use this for initialization
+	void Start () {
+	
+		for (float i = 3; i < 6; i+=0.5f) {
+		
+			for(float j = -4; j < 3; j+=1.0f){
+			
+				GameObject e = (GameObject) Instantiate(enemyPrefab, new Vector3(j,i,0), Quaternion.identity);
+
+			}
+
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
